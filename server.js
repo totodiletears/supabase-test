d025fbf2537fc14ab2611ef0bab1ec508e9dd8f5
totodiletears <toto.ilebinted@gmail.com>
2022-07-14 15:38:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
 const express = require('express');
 const app = express();
 
@@ -24,6 +27,6 @@ app.get('/user/:address', async (req, res) => {
 girl.on("Minted", girlMinted);
 girl.on("Transfer", girlTransferred);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("server running on port 3000");
 })
