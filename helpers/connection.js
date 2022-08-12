fbb904d4117375b13333fb0ef356457f5cbaf465
totodiletears <toto.ilebinted@gmail.com>
2022-08-11 18:45:29 -0700

    prep move

--- helpers/connection.js
+++ helpers/connection.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const ethers = require('ethers');
 const abi = require('../abi/girl.json');
+const GIRL_CONTRACT = require('../config');
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.JSON_PROVIDER);
-const girl = new ethers.Contract(process.env.GIRL_CONTRACT, abi.abi, provider);
+const girl = new ethers.Contract(GIRL_CONTRACT, abi.abi, provider);
 
 module.exports = provider;
 module.exports = girl;
